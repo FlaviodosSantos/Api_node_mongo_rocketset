14bdf502bc68c2b46e22a290948b9c292259ff89
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2023-04-20 22:17:33 -0300

    rota reset_password (26:37)

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -116,5 +116,35 @@ router.post('/forgot_password', async(req, res)=>{
     }
 })
 
+router.post('/reset_password', async (req, res) => {
+  const { email, token, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email })
+      .select('+passwordResetToken passwordResetExpires');
+
+    if (!user)
+      return res.status(400).send({ error: 'User not found' });
+
+    if (token !== user.passwordResetToken)
+      return res.status(400).send({ error: 'Token is not valid' });
+
+    const now = new Date();
+
+    if (now > user.passwordResetExpires)
+      return res.status(400).send({ error: 'Token expired, generate a new one' });
+
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    user.password = password;
+
+    await user.save();
+
+    res.send();
+  } catch (err) {
+    res.status(400).send({ error: 'Cannot reset password, try again' });
+  }
+});
+
 module.exports = app => app.use('/auth', router)
 
